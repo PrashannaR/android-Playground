2c57d6a2035999b4417fbac5c1993acff3e3afae
Prashanna <prashanna.rajbhandari1@gmail.com>
2024-06-05 00:05:03 +0545

    feat: slight ui modification

--- app/src/main/java/com/prashanna/androidplayground/posts/Presentation/view/PostsList.kt
+++ app/src/main/java/com/prashanna/androidplayground/posts/Presentation/view/PostsList.kt
@@ -2,10 +2,15 @@ package com.prashanna.androidplayground.posts.Presentation.view
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prashanna.androidplayground.posts.Presentation.ViewModel.PostsViewModel
 
 @Composable
@@ -24,9 +29,17 @@ fun PostsList(viewModel: PostsViewModel) {
     } else {
         LazyColumn {
             items(posts.value) { post ->
-                Column {
-                    Text(post.title)
-                    Text(post.body)
+                Column(modifier = Modifier.padding(16.dp)){
+                    Text(post.title,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        post.body,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal
+
+                        )
                 }
             }
         }
