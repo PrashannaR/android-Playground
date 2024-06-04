efdc09025d7914522553f978fa0fd3d220e7f6c5
Prashanna <prashanna.rajbhandari1@gmail.com>
2024-06-04 23:14:34 +0545

    feat: response model init

---
+++ app/src/main/java/com/prashanna/androidplayground/posts/Domain/Model/PostsResponseModel.kt
@@ -0,0 +1,9 @@
+package com.prashanna.androidplayground.posts.Domain.Model
+
+data class PostsResponseModel(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
+ 
